3c488fb591c820faa1c03b5a0cc79c0744d4547d
anubhav-aryan <aryananubhav08@gmail.com>
2023-10-02 13:30:28 +0530

    feat: texteditor

---
+++ frontend/src/pages/texteditor.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { createEditor } from 'slate'
+import { Slate, Editable, withReact } from 'slate-react'
+
+const texteditor = () => {
+  return (
+    <div>
+      Hello 
+    </div>
+  )
+}
+
+export default texteditor
