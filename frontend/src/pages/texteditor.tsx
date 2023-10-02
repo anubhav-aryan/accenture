b862f7d480a485b56b27f2646efe48cc61cd6306
anubhav-aryan <aryananubhav08@gmail.com>
2023-10-03 02:55:52 +0530

    feat: custom formatting

--- frontend/src/pages/texteditor.tsx
+++ frontend/src/pages/texteditor.tsx
@@ -1,12 +1,9 @@
 import { BaseEditor, Descendant } from "slate";
 import { ReactEditor } from "slate-react";
-// Import React dependencies.
-import React, { useState } from "react";
-// Import the Slate editor factory.
+import React, { useState, useCallback } from "react";
 import { createEditor } from "slate";
-
-// Import the Slate components and React plugin.
 import { Slate, Editable, withReact } from "slate-react";
+import { Editor, Transforms, Element } from "slate";
 
 type CustomElement = { type: "paragraph"; children: CustomText[] };
 type CustomText = { text: string };
@@ -29,20 +26,51 @@ const initialValue = [
 const texteditor = () => {
   const [editor] = useState(() => withReact(createEditor()));
 
+  const renderElement = useCallback((props) => {
+    switch (props.element.type) {
+      case "code":
+        return <CodeElement {...props} />;
+      default:
+        return <DefaultElement {...props} />;
+    }
+  }, []);
+
   return (
-    <>
-      <Slate editor={editor} initialValue={initialValue}>
-        <Editable
-          onKeyDown={(event) => {
-            if (event.key === "&") {
-              event.preventDefault();
-              editor.insertText("and");
-            }
-          }}
-        />
-      </Slate>
-    </>
+    <Slate editor={editor} initialValue={initialValue}>
+      <Editable
+        renderElement={renderElement}
+        onKeyDown={(event) => {
+          if (event.key === "`" && event.ctrlKey) {
+            event.preventDefault();
+            // Determine whether any of the currently selected blocks are code blocks.
+            const [match] = Editor.nodes(editor, {
+              match: (n) => n.type === "code",
+            });
+            // Toggle the block type depending on whether there's already a match.
+            Transforms.setNodes(
+              editor,
+              { type: match ? "paragraph" : "code" },
+              {
+                match: (n) => Element.isElement(n) && Editor.isBlock(editor, n),
+              },
+            );
+          }
+        }}
+      />
+    </Slate>
   );
+};
+
+const CodeElement = (props) => {
+  return (
+    <pre {...props.attributes}>
+      <code>{props.children}</code>
+    </pre>
+  );
+};
+
+const DefaultElement = (props) => {
+  return <p {...props.attributes}>{props.children}</p>;
 };
 
 export default texteditor;
