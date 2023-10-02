8fe30da09b3f3b1c3b23aad242aabac5b29509e4
anubhav-aryan <aryananubhav08@gmail.com>
2023-10-03 03:36:34 +0530

    feat: bold

--- frontend/src/pages/texteditor.tsx
+++ frontend/src/pages/texteditor.tsx
@@ -1,12 +1,11 @@
 import { BaseEditor, Descendant } from "slate";
-import { ReactEditor } from "slate-react";
+import { ReactEditor, RenderLeafProps } from "slate-react";
 import React, { useState, useCallback } from "react";
-import { createEditor } from "slate";
+import { createEditor, Editor, Transforms, Element } from "slate";
 import { Slate, Editable, withReact } from "slate-react";
-import { Editor, Transforms, Element } from "slate";
 
-type CustomElement = { type: "paragraph"; children: CustomText[] };
-type CustomText = { text: string };
+type CustomElement = { type: "paragraph" | "code"; children: CustomText[] };
+type CustomText = { text: string; type?: string };
 
 declare module "slate" {
   interface CustomTypes {
@@ -16,48 +15,73 @@ declare module "slate" {
   }
 }
 
-const initialValue = [
-  {
-    type: "paragraph",
-    children: [{ text: "A line of text in a paragraph." }],
-  },
-];
+const renderElement = (props) => {
+  switch (props.element.type) {
+    case "code":
+      return <CodeElement {...props} />;
+    default:
+      return <DefaultElement {...props} />;
+  }
+};
 
-const texteditor = () => {
+const TextEditor = () => {
   const [editor] = useState(() => withReact(createEditor()));
 
-  const renderElement = useCallback((props) => {
-    switch (props.element.type) {
-      case "code":
-        return <CodeElement {...props} />;
-      default:
-        return <DefaultElement {...props} />;
-    }
+  const renderLeaf = useCallback((props: RenderLeafProps) => {
+    return <Leaf {...props} />;
   }, []);
+
+  const initialValue: CustomElement[] = [
+    {
+      type: "paragraph",
+      children: [{ text: "A line of text in a paragraph." }],
+    },
+  ];
 
   return (
     <Slate editor={editor} initialValue={initialValue}>
       <Editable
         renderElement={renderElement}
+        renderLeaf={renderLeaf}
         onKeyDown={(event) => {
-          if (event.key === "`" && event.ctrlKey) {
-            event.preventDefault();
-            // Determine whether any of the currently selected blocks are code blocks.
-            const [match] = Editor.nodes(editor, {
-              match: (n) => n.type === "code",
-            });
-            // Toggle the block type depending on whether there's already a match.
-            Transforms.setNodes(
-              editor,
-              { type: match ? "paragraph" : "code" },
-              {
-                match: (n) => Element.isElement(n) && Editor.isBlock(editor, n),
-              },
-            );
+          if (!event.ctrlKey) {
+            return;
+          }
+
+          switch (event.key) {
+            case "`": {
+              event.preventDefault();
+              const [match] = Editor.nodes(editor, {
+                match: (n) => n.type === "code",
+              });
+              Transforms.setNodes(
+                editor,
+                { type: match ? "paragraph" : "code" },
+                { match: (n) => Editor.isBlock(editor, n) }
+              );
+              break;
+            }
+
+            case "b": {
+              event.preventDefault();
+              Editor.addMark(editor, "bold", true);
+              break;
+            }
           }
         }}
       />
     </Slate>
+  );
+};
+
+const Leaf = (props) => {
+  return (
+    <span
+      {...props.attributes}
+      style={{ fontWeight: props.leaf.bold ? "bold" : "normal" }}
+    >
+      {props.children}
+    </span>
   );
 };
 
@@ -73,4 +97,4 @@ const DefaultElement = (props) => {
   return <p {...props.attributes}>{props.children}</p>;
 };
 
-export default texteditor;
+export default TextEditor;
