980bd097c665093c63c5ef75955d734b4512c65e
alisha0704 <alishabandyopadhyay07@gmail.com>
2023-10-06 03:59:49 +0530

    fix: removed text editor

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -11,9 +11,6 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Header />
-      <Link href="/richtext">
-        Text Editor 
-      </Link>
     </>
   );
 }
