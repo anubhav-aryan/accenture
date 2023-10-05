7e0be9729453249b3d0307d3b9480f6c05730909
anubhav-aryan <aryananubhav08@gmail.com>
2023-10-05 21:26:14 +0530

    fix: header

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -8,15 +8,15 @@ const Header = () => {
     <>
       <header className="body-font text-black">
         <div className="container mx-auto flex flex-col flex-wrap items-center p-5 md:flex-row">
-          <a className="title-font mb-4 flex items-center font-medium text-white md:mb-0 border-black border-4 bg-emerald-300">
+          <a className="title-font mb-4 flex items-center border-4 border-black bg-emerald-300 font-medium text-white md:mb-0">
             <Image src="/logo.svg" alt="logo" width={50} height={50} />
-            <span className="ml-3 text-xl text-black pr-3">ScriptScanner</span>
+            <span className="ml-3 pr-3 text-xl text-white">ScriptScanner</span>
           </a>
-          <nav className="flex flex-wrap items-center justify-center text-base md:ml-auto md:mr-auto">
-            <a className="mr-5 hover:text-white">Home</a>
-            <a className="mr-5 hover:text-white">Product</a>
-            <a className="mr-5 hover:text-white">FAQ</a>
-            <a className="mr-5 hover:text-white">About Us</a>
+          <nav className="flex flex-wrap items-center justify-center text-lg font-medium text-gray-500 md:ml-auto md:mr-auto">
+            <a className="mx-8">Home</a>
+            <a className="mx-8">Product</a>
+            <a className="mx-8">FAQ</a>
+            <a className="mx-8">About Us</a>
           </nav>
           <SigninButton />
         </div>
