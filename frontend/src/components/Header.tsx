68c72cd121bc9a39998c227a63925c7bbcd44e49
anubhav-aryan <aryananubhav08@gmail.com>
2023-10-05 23:04:21 +0530

    fix:header

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -5,23 +5,26 @@ import Image from "next/image";
 
 const Header = () => {
   return (
-    <>
-      <header className="body-font text-black">
-        <div className="container mx-auto flex flex-col flex-wrap items-center p-5 md:flex-row">
-          <a className="title-font mb-4 flex items-center border-4 border-black bg-emerald-300 font-medium text-white md:mb-0">
+    <div className="body-font text-black border-b-4">
+      <div className="mx-auto flex flex-row items-center justify-between p-5 md:flex-row">
+        <div className="title-font mb-4 flex items-center border-4 border-black bg-emerald-300 font-medium text-white md:mb-0">
+          <div>
             <Image src="/logo.svg" alt="logo" width={50} height={50} />
-            <span className="ml-3 pr-3 text-xl text-white">ScriptScanner</span>
-          </a>
-          <nav className="flex flex-wrap items-center justify-center text-lg font-medium text-gray-500 md:ml-auto md:mr-auto">
-            <a className="mx-8">Home</a>
-            <a className="mx-8">Product</a>
-            <a className="mx-8">FAQ</a>
-            <a className="mx-8">About Us</a>
-          </nav>
+          </div>
+          <div className="ml-3 pr-3 text-2xl text-white py-4">ScriptScanner</div>
+        </div>
+        <div className="flex-grow"></div>{" "}
+        <div className="flex items-end justify-end px-6 text-lg font-medium text-gray-500">
+          <div className="px-8">Home</div>
+          <div className="px-8">Product</div>
+          <div className="px-8">FAQ</div>
+          <div className="px-8">About Us</div>
+        </div>
+        <div>
           <SigninButton />
         </div>
-      </header>
-    </>
+      </div>
+    </div>
   );
 };
 
