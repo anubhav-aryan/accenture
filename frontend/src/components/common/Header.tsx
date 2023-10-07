11e00f8488622dfad9034d01e7c2d7e0b7fc2bc8
viraj-s15 <veer15102003@gmail.com>
2023-10-07 14:21:25 +0530

    feat: added progress bar

--- frontend/src/components/common/Header.tsx
+++ frontend/src/components/common/Header.tsx
@@ -1,31 +1,57 @@
-import React from "react";
+// Header.js
+import React, { useEffect, useState } from "react";
 import { signIn, signOut, useSession } from "next-auth/react";
 import SigninButton from "../uncommon/Signinbutton";
 import Image from "next/image";
 
 const Header = () => {
+  const [scrollPercentage, setScrollPercentage] = useState(0);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const windowHeight = window.innerHeight;
+      const scrollY = window.scrollY;
+      const documentHeight = document.documentElement.scrollHeight;
+      const newScrollPercentage = (scrollY / (documentHeight - windowHeight)) * 100;
+      setScrollPercentage(newScrollPercentage);
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
   return (
-    <div className="body-font text-black border-b-4">
-      <div className="mx-auto flex flex-row items-center justify-between p-2 md:flex-row">
-        <div className="title-font mb-4 flex items-center border-4 border-black bg-emerald-300 font-medium text-white md:mb-0">
+    <div className="sticky top-0 bg-white z-10">
+      <div className="mx-auto flex flex-row items-center justify-between p-2 md:flex-row relative">
+        <div className="flex items-center space-x-3">
           <div>
             <Image src="/logo.svg" alt="logo" width={50} height={50} />
           </div>
-          <div className="ml-3 pr-3 text-2xl text-white py-3">ScriptScanner</div>
+          <div className="text-2xl text-white py-3 font-medium">
+            ScriptScanner
+          </div>
         </div>
-        <div className="flex-grow"></div>{" "}
-        <div className="flex items-end justify-end px-6 text-xl font-medium text-gray-700 ">
-          <div className="px-8 hover:text-gray-500">Home</div>
-          <div className="px-8 hover:text-gray-500">Product</div>
-          <div className="px-8 hover:text-gray-500">FAQ</div>
-          <div className="px-8 hover:text-gray-500">About Us</div>
+        <div className="flex-grow"></div>
+        <div className="flex items-end justify-end space-x-6 text-xl font-medium text-gray-700">
+          <div className="hover:text-gray-500">Home</div>
+          <div className="hover:text-gray-500">Product</div>
+          <div className="hover:text-gray-500">FAQ</div>
+          <div className="hover:text-gray-500">About Us</div>
         </div>
-        <div>
+        <div className="ml-2">
           <SigninButton />
         </div>
       </div>
+      <div
+        className="bg-emerald-300 h-1 absolute bottom-0 left-0 right-0"
+        style={{ width: `${scrollPercentage}%` }}
+      ></div>
     </div>
   );
 };
 
 export default Header;
+
