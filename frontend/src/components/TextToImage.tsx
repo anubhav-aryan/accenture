91527391a7f3c0bb4fd9a889b698bef9dcfa2f06
anubhav-aryan <aryananubhav08@gmail.com>
2023-10-10 19:39:47 +0530

    fix: image prediction ui

--- frontend/src/components/TextToImage.tsx
+++ frontend/src/components/TextToImage.tsx
@@ -2,6 +2,7 @@
 import { useState } from "react";
 import { useForm } from "react-hook-form";
 import { client } from "@gradio/client";
+import Image from "next/image";
 
 interface PredictionResult {
   data: string[];
@@ -15,7 +16,8 @@ export default function ImagePrediction() {
   const onSubmit = async (data: { inputText: string }) => {
     try {
       const app = await client(
-        "https://veer15-stabilityai-stable-diffusion-2-1.hf.space/" ,{},
+        "https://veer15-stabilityai-stable-diffusion-2-1.hf.space/",
+        {},
       );
       const result = await app.predict("/predict", [data.inputText]);
       setPredictionResult(result);
@@ -26,36 +28,53 @@ export default function ImagePrediction() {
 
   return (
     <div>
-      <div className="justify-center text-center text-5xl text-black pb-14">
+      <div className="justify-center pb-14 text-center text-5xl text-black">
         Image Prediction
       </div>
-      <div className="flex justify-center">
+      <div className="flex justify-around">
         <form onSubmit={handleSubmit(onSubmit)}>
-          <label>
-            Input Text:
-            <input {...register("inputText")} type="text" />
+          <label className="sr-only mb-2 text-sm font-medium text-gray-900 dark:text-white">
+            {" "}
+            Search{" "}
           </label>
-          <button type="submit" className="">
-            <a className="group relative overflow-hidden rounded-lg border border-gray-100 bg-gray-100 px-5 py-3 font-medium text-gray-600 shadow-inner">
-              <span className="ease absolute left-0 top-0 h-0 w-0 border-t-2 border-gray-600 transition-all duration-200 group-hover:w-full"></span>
-              <span className="ease absolute bottom-0 right-0 h-0 w-0 border-b-2 border-gray-600 transition-all duration-200 group-hover:w-full"></span>
-              <span className="ease absolute left-0 top-0 h-0 w-full bg-gray-600 transition-all delay-200 duration-300 group-hover:h-full"></span>
-              <span className="ease absolute bottom-0 left-0 h-0 w-full bg-gray-600 transition-all delay-200 duration-300 group-hover:h-full"></span>
-              <span className="absolute inset-0 h-full w-full bg-emerald-500 opacity-0 delay-300 duration-300 group-hover:opacity-100"></span>
-              <span className="ease relative transition-colors delay-200 duration-300 group-hover:text-white">
-                Predict
-              </span>
-            </a>
-          </button>
+          <div className="p-10">
+            Input Text:
+            <input
+              {...register("inputText")}
+              className=""
+              type="text"
+              placeholder="  Input your request here"
+              required
+            />
+          </div>
+          <div>
+            <button
+              type="submit"
+              className="items-center justify-center p-10 px-56"
+            >
+              <a className="group relative overflow-hidden rounded-lg border border-gray-100 bg-gray-100 px-5 py-3 font-medium text-gray-600 shadow-inner">
+                <span className="ease absolute left-0 top-0 h-0 w-0 border-t-2 border-gray-600 transition-all duration-200 group-hover:w-full"></span>
+                <span className="ease absolute bottom-0 right-0 h-0 w-0 border-b-2 border-gray-600 transition-all duration-200 group-hover:w-full"></span>
+                <span className="ease absolute left-0 top-0 h-0 w-full bg-gray-600 transition-all delay-200 duration-300 group-hover:h-full"></span>
+                <span className="ease absolute bottom-0 left-0 h-0 w-full bg-gray-600 transition-all delay-200 duration-300 group-hover:h-full"></span>
+                <span className="absolute inset-0 h-full w-full bg-emerald-500 opacity-0 delay-300 duration-300 group-hover:opacity-100"></span>
+                <span className="ease relative transition-colors delay-200 duration-300 group-hover:text-white">
+                  Predict
+                </span>
+              </a>
+            </button>
+          </div>
         </form>
 
         {predictionResult && (
           <div className="flex flex-col items-center justify-center">
             <h2>Prediction Result:</h2>
-            <img
+            <Image
               src={predictionResult.data[0]}
               alt="Predicted Image"
-              className="mt-4" // Add margin-top for spacing
+              width={500}
+              height={500}
+              className="mt-4"
             />
           </div>
         )}
