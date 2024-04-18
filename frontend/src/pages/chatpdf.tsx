c5f26733141410e2bfe15b18b1c1ab547a090231
Alisha Bandyopadhyay <alishabandyopadhyay07@gmail.com>
2024-04-18 13:01:25 +0530

    feat: added chatpdf page

---
+++ frontend/src/pages/chatpdf.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import { Button } from '@/components/ui/button';
+
+type chatpdfProps = {
+    
+};
+
+const chatpdf:React.FC<chatpdfProps> = () => {
+    
+    return(
+        <>
+        <div className="w-screen min-h-screen bg-gradient-to-r from-rose-100 to-teal-300">
+         <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
+           <div className="flex flex-col items-center text-center">
+             <div className="flex items-center">
+               <h1 className="mr-3 text-5xl font-serif font-bold">Chat with any PDF</h1>
+               </div>
+               <div className='flex mt-2 font-serif'>
+                <Button> Go to Chats</Button>
+               </div>
+               <p className="max-w-xl mt-1 text-lg text-slate-600 font-serif">
+                Join millions of students, researchers and professionals to instantly
+                answer questions and understand research with AI
+              </p>
+               </div>
+               </div>
+               </div>
+       </>
+    )
+}
+export default chatpdf;
