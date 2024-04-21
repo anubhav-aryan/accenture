98d2e2721167b4519023432ac9264b573627de6d
Alisha Bandyopadhyay <alishabandyopadhyay07@gmail.com>
2024-04-21 11:53:30 +0530

    feat: added pinecone

--- frontend/src/pages/chatpdf.tsx
+++ frontend/src/pages/chatpdf.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { Button } from '@/components/ui/button';
-
+import FileUpload from '@/components/FileUpload';
+import Provider from '@/components/Provider';
+import { Toaster } from 'react-hot-toast';
 type chatpdfProps = {
     
 };
@@ -9,6 +11,8 @@ const chatpdf:React.FC<chatpdfProps> = () => {
     
     return(
         <>
+        <Provider>
+          <Toaster/>
         <div className="w-screen min-h-screen bg-gradient-to-r from-rose-100 to-teal-300">
          <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
            <div className="flex flex-col items-center text-center">
@@ -22,9 +26,14 @@ const chatpdf:React.FC<chatpdfProps> = () => {
                 Join millions of students, researchers and professionals to instantly
                 answer questions and understand research with AI
               </p>
+              <div className="w-full mt-4">
+                <FileUpload/>
+          </div>
                </div>
                </div>
                </div>
+          
+               </Provider>
        </>
     )
 }
