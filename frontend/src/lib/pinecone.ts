6427bad5633f76e0b7b3745c01ef75826ba40191
Alisha Bandyopadhyay <alishabandyopadhyay07@gmail.com>
2024-04-21 12:23:02 +0530

    feat: added embeddings

--- frontend/src/lib/pinecone.ts
+++ frontend/src/lib/pinecone.ts
@@ -1,7 +1,7 @@
 import { Pinecone, PineconeRecord } from "@pinecone-database/pinecone";
 import { downloadFromS3 } from "./s3-server";
 import { PDFLoader } from "langchain/document_loaders/fs/pdf";
-
+import  {Document, RecursiveCharacterTextSplitter} from '@pinecone-database/doc-splitter' 
 export const getPineconeClient = () => {
     return new Pinecone({
       apiKey: process.env.PINECONE_API_KEY!,
@@ -25,5 +25,31 @@ export const getPineconeClient = () => {
 
    const loader = new PDFLoader(file_name);
    const pages= (await loader.load()) as PDFPage[] ;
+   const documents = await Promise.all(pages.map(prepareDocument));
+
+
+
    return pages;
+  }
+  
+  export const truncateStringByBytes = (str: string, bytes: number) => {
+    const enc = new TextEncoder();
+    return new TextDecoder("utf-8").decode(enc.encode(str).slice(0, bytes));
+  };
+  
+  async function prepareDocument(page: PDFPage) {
+    let { pageContent, metadata } = page;
+    pageContent = pageContent.replace(/\n/g, "");
+    // split the docs
+    const splitter = new RecursiveCharacterTextSplitter();
+    const docs = await splitter.splitDocuments([
+      new Document({
+        pageContent,
+        metadata: {
+          pageNumber: metadata.loc.pageNumber,
+          text: truncateStringByBytes(pageContent, 36000),
+        },
+      }),
+    ]);
+    return docs;
   }
